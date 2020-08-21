d3a4d2987bd278053e425d8f9d114591ed37ac60
Mohamed <rayyan.mohamed151@gmail.com>
2020-08-21 15:11:39 -0700

    updated scale class

--- resources/Scale.py
+++ resources/Scale.py
@@ -14,7 +14,7 @@ class Scale:
 	def checkWeight(self):	#execute after closing
 		if self.getWeight() >= self.limit:
 			print("take out the trash")
-			Speaker.trashDay()
+			Speaker.fullTrash()
 			return 0
 
 	def getWeight(self):
