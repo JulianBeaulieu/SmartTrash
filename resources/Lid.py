5fe2385f0ab774b0958d6f4a5e5ae9a05fce8523
Mohamed Rayyan <rayyan.mohamed151@gmail.com>
2020-08-20 19:01:02 -0700

    made code look nicer

--- resources/Lid.py
+++ resources/Lid.py
@@ -9,8 +9,8 @@ class Lid:
     #lidIsOpen = False
 
     def __init__(self):
-
-        self.lidIsOpen =False
+        #self.dontCloseYet = True
+        self.lidIsOpen = False
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(self.inputPin1, GPIO.OUT)
@@ -19,38 +19,41 @@ class Lid:
         GPIO.output(self.inputPin1, GPIO.LOW)
         GPIO.output(self.inputPin2, GPIO.LOW)
         self.motorSpeed = GPIO.PWM(self.powerLevelPin,1000)
+        self.motorSpeed.ChangeDutyCycle(100)
         self.motorSpeed.start(0)
 
 
     def openLid(self):
+        #self.dontCloseYet = False
         if not self.lidIsOpen:
         	print("openning lid")
-	        self.open(100)
+	        self.open()
 	        sleep(2)
 	        self.turn_off()
 	        print("finished openning lid")
 	        self.lidIsOpen = True
+        sleep(2)
+        #self.dontCloseYet = True
+
 
     def closeLid(self):
         if self.lidIsOpen:
         	print("closing lid")
-	        self.close(100)
-	        sleep(3)
+	        self.close()
+	        sleep(2)
 	        self.turn_off()
 	        print("finished closing lid")
-            	self.lidIsOpen = False
+	        self.lidIsOpen = False
 
 
     def turn_off(self):
         GPIO.output(self.inputPin1,GPIO.LOW)
         GPIO.output(self.inputPin2,GPIO.LOW)
 
-    def open(self, speed):
-        self.motorSpeed.ChangeDutyCycle(speed)
+    def open(self):
         GPIO.output(self.inputPin1,GPIO.LOW)
         GPIO.output(self.inputPin2,GPIO.HIGH)
 
-    def close(self, speed):
-        self.motorSpeed.ChangeDutyCycle(speed)
+    def close(self):
         GPIO.output(self.inputPin1,GPIO.HIGH)
         GPIO.output(self.inputPin2,GPIO.LOW)
