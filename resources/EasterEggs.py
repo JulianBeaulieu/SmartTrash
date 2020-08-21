2a8857d79e5f7edd947f23eb191ac0ab1163025b
Julian Beaulieu <julianbeaulieu@yahoo.com>
2020-08-21 15:04:59 -0700

    asdf

--- resources/EasterEggs.py
+++ resources/EasterEggs.py
@@ -19,3 +19,28 @@ class KobeBryant:
 		else:
 			Speaker.fullTrash()
 
+class Halloween:
+	@staticmethod
+	def play():
+		path = 'Recordings/KobeMode/'
+		scale = Scale()
+		lid = Lid()
+		oldWeight =scale.getWeight()
+		lid.openLid()
+		Speaker.playSound(path + 'buzzer.m4a')
+		lid.closeLid()
+
+		if scale.getWeight() >oldWeight:
+			Speaker.playSound('cheering.mp3')
+		else:
+			Speaker.fullTrash()
+
+class Music:
+	@staticmethod
+	def play():
+		os.system('mpg123 http://ice1.somafm.com/u80s-128-mp3')
+
+class System:
+	@staticmethod
+	def reboot():
+		os.system('sudo reboot')
