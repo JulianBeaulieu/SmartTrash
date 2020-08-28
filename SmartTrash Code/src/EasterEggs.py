36102689145351974cde213e37f800b011c3fd7b
Julian Beaulieu <julianbeaulieu@yahoo.com>
2020-08-27 17:46:59 -0700

    Update EasterEggs.py

--- SmartTrash Code/src/EasterEggs.py
+++ SmartTrash Code/src/EasterEggs.py
@@ -25,49 +25,37 @@ class KobeBryant:
 			Speaker.playSound(path + 'boo.mp3')
 
 class HalloweenLidMovementThread (threading.Thread):
-	def __init__(self, threadID, name, counter, lid):
+	def __init__(self, threadID, lid):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
-		self.name = name
-		self.counter = counter
 		self.lid = lid
 
 	def run(self):
-		print "Starting " + self.name
-		# Get lock to synchronize threads
-		threadLock.acquire()
+		print "Starting Lid Movement"
 
-		#sleep(1)
 		self.lid.openLid()
 		self.lid.closeLid()
-		# Free lock to release next thread
-		threadLock.release()
 
-		# Free lock to release next thread
-		#threadLock.release()
+		print "Ending Lid Movement"
+
 class HalloweenScreamThread (threading.Thread):
-	def __init__(self, threadID, name, counter, lid):
+	def __init__(self, threadID):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
-		self.name = name
-		self.counter = counter
-		self.lid = lid
 
 	def run(self):
-		print "Starting " + self.name
-		# Get lock to synchronize threads
-		threadLock.acquire()
+		print "Starting Screaming"
 
 		Speaker.playSound('../Recordings/HalloweenMode/scream.mp3')
-		# Free lock to release next thread
-		threadLock.release()
+
+		print "Ending Screaming"
 
 class Halloween:
 	@staticmethod
 	def play(lid):
 		# Create new threads
-		thread1 = HalloweenLidMovementThread(1, "Thread-1", 1, lid)
-		thread2 = HalloweenScreamThread(2, "Thread-2", 2, lid)
+		thread1 = HalloweenLidMovementThread(1, lid)
+		thread2 = HalloweenScreamThread(2)
 
 		# Start new Threads
 		thread1.start()
