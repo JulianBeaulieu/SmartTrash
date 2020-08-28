0a4fc7c977e39a3a0294586d57eed28aa1a0dcbc
Julian Beaulieu <julianbeaulieu@yahoo.com>
2020-08-27 17:17:46 -0700

    Update EasterEggs.py

--- SmartTrash Code/src/EasterEggs.py
+++ SmartTrash Code/src/EasterEggs.py
@@ -23,24 +23,25 @@ class KobeBryant:
 			Speaker.playSound(path + 'boo.mp3')
 
 class HalloweenThread (threading.Thread):
-   def __init__(self, threadID, name, counter):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-      self.counter = counter
-   def run(self):
-      print "Starting " + self.name
-      # Get lock to synchronize threads
-      threadLock.acquire()
-      print_time(self.name, self.counter, 3)
-      # Free lock to release next thread
-      threadLock.release()
+	def __init__(self, threadID, name, counter):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.counter = counter
+
+	def run(self):
+		print "Starting " + self.name
+		# Get lock to synchronize threads
+		threadLock.acquire()
+		print_time(self.name, self.counter, 3)
+		# Free lock to release next thread
+		threadLock.release()
 
 	def print_time(threadName, delay, counter):
-	   while counter:
-	      time.sleep(delay)
-	      print "%s: %s" % (threadName, time.ctime(time.time()))
-	      counter -= 1
+		while counter:
+			time.sleep(delay)
+			print "%s: %s" % (threadName, time.ctime(time.time()))
+			counter -= 1
 
 
 class Halloween:
