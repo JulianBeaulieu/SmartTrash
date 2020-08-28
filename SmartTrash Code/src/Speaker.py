8d57744f3cbd457ffd6cbf88c78cf5e2d74d7815
Julian Beaulieu <julianbeaulieu@yahoo.com>
2020-08-27 18:53:25 -0700

    Update Speaker.py

--- SmartTrash Code/src/Speaker.py
+++ SmartTrash Code/src/Speaker.py
@@ -6,14 +6,14 @@ from Language import Language
 class Speaker:
 	@staticmethod
 	def fullTrash():
-		language = Langauge()
+		language = Language()
 		path ='Recordings/FullTrash/' + language.getLanguage() + '/'
 		file = str(random.choice(os.listdir(path)))
 		os.system('mpg123 ' + path + file)
 
 	@staticmethod
 	def trashDay():
-		language = Langauge()
+		language = Language()
 		path ='Recordings/TrashDay/' + language.getLanguage() + '/'
 		file = str(random.choice(os.listdir(path)))
 		os.system('mpg123 ' + path + file)
