2eb5cd00e919e596ebf6781180494433855c2467
Julian Beaulieu <julianbeaulieu@yahoo.com>
2020-08-27 22:12:33 -0700

    Update Speaker.py

--- SmartTrash Code/src/Speaker.py
+++ SmartTrash Code/src/Speaker.py
@@ -2,6 +2,7 @@ import os
 import sys
 import random
 from Language import Language
+from soundplayer import SoundPlayer
 
 class Speaker:
 	@staticmethod
@@ -9,15 +10,21 @@ class Speaker:
 		language = Language()
 		path ='../Recordings/FullTrash/' + language.getLanguage() + '/'
 		file = str(random.choice(os.listdir(path)))
-		os.system('mpg123 ' + path + file)
+		#os.system('mpg123 ' + path + file)
+		p = SoundPlayer(path + file, 1)
+		p.play(1)
 
 	@staticmethod
 	def trashDay():
 		language = Language()
 		path ='../Recordings/TrashDay/' + language.getLanguage() + '/'
 		file = str(random.choice(os.listdir(path)))
-		os.system('mpg123 ' + path + file)
+		#os.system('mpg123 ' + path + file)
+		p = SoundPlayer(path + file, 1)
+		p.play(1)
 
 	@staticmethod
 	def playSound(path):
-		os.system('mpg123 ' + path)
+		#os.system('mpg123 ' + path)
+		p = SoundPlayer(path, 1)
+		p.play(1)
